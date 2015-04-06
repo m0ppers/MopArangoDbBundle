5b26601a738c772959047b661c91e6a30b923080
Mario Mueller <mueller@freshcells.de>
2015-04-06 10:04:31 +0200

    * Reformat some code * Cached some function calls into variables * pulled logger reference initialization out of the loop

--- DependencyInjection/MopArangoDbExtension.php
+++ DependencyInjection/MopArangoDbExtension.php
@@ -2,12 +2,12 @@
 
 namespace Mop\ArangoDbBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -23,38 +23,63 @@ class MopArangoDbExtension extends Extension
     {
         $configuration = new Configuration($container->getParameter('kernel.debug'));
         $config = $this->processConfiguration($configuration, $configs);
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        if (true === $container->getParameter('kernel.debug')) {
+        $isDebug = $container->getParameter('kernel.debug');
+        $hasConnections = !empty($config['connections']);
+        $hasDefaultConnection = !empty($config['default_connection']);
+        $hasFosUserBundle = !empty($config['fos']);
+        $logger = new Reference('mop_arangodb.data_collector');
+
+        if ($isDebug) {
             $loader->load('logger.xml');
         }
-        if (isset($config['connections'])) {
-            if (!isset($config['default_connection'])) {
+
+        if ($hasConnections) {
+
+            if (!$hasDefaultConnection) {
                 $keys = array_keys($config['connections']);
                 $config['default_connection'] = reset($keys);
+                if (!empty($config['default_connection'])) {
+                    $hasDefaultConnection = true;
+                }
             }
+
             $loader->load('connection.xml');
             foreach ($config['connections'] as $name => $connection) {
                 $arguments = array_merge(array($name), array_values($connection));
-                
-                $logger = new Reference('mop_arangodb.data_collector');
-                if (true === $container->getParameter('kernel.debug')) {
-                    $container->getDefinition('mop_arangodb.connection_factory')->addMethodCall('addLogger', array($logger));
+
+                if ($isDebug) {
+                    $container
+                        ->getDefinition('mop_arangodb.connection_factory')
+                        ->addMethodCall(
+                            'addLogger',
+                            array($logger)
+                        );
                 }
                 $container
-                    ->setDefinition('mop_arangodb.connections.'.$name, new DefinitionDecorator('mop_arangodb.connection'))
+                    ->setDefinition(
+                        'mop_arangodb.connections.' . $name,
+                        new DefinitionDecorator('mop_arangodb.connection')
+                    )
                     ->setArguments($arguments);
             }
         }
 
 
-        if ($config['default_connection']) {
-            $container->setAlias('mop_arangodb.default_connection', 'mop_arangodb.connections.'.$config['default_connection']);
+        if ($hasDefaultConnection) {
+            $container->setAlias(
+                'mop_arangodb.default_connection',
+                'mop_arangodb.connections.' . $config['default_connection']
+            );
         }
 
-        if (isset($config['fos'])) {
+        if ($hasFosUserBundle) {
             $container->setParameter('mop_arangodb.fos.collection', $config['fos']['collection']);
-            $container->setAlias('mop_arangodb.fos.connection', 'mop_arangodb.connections.'.$config['fos']['connection']);
+            $container->setAlias(
+                'mop_arangodb.fos.connection',
+                'mop_arangodb.connections.' . $config['fos']['connection']
+            );
 
             $loader->load('fos.xml');
         }
