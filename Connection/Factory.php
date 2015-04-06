3ced1a9cb352a45ea079433e4df6cf4c17e4d2b5
Mario Mueller <mueller@freshcells.de>
2015-04-06 09:59:34 +0200

    Changed factory to directly recognize any added logger. As there is no remove function, the only state possible, after adding at least one logger, is an array with a size of at least one. The bool comparison should be way faster than any count/sizeof function.

--- Connection/Factory.php
+++ Connection/Factory.php
@@ -1,24 +1,51 @@
 <?php
 namespace Mop\ArangoDbBundle\Connection;
 
-use Mop\ArangoDbBundle\LoggerInterface;
+use Mop\ArangoDbBundle\DataCollector\LoggerInterface;
 use triagens\ArangoDb\Connection;
 
+/**
+ * @package Mop\ArangoDbBundle\Connection
+ */
 class Factory
 {
+    /**
+     *
+     * @var LoggerInterface[]
+     */
     private $loggers = array();
 
+    /**
+     * @var bool
+     */
+    private $hasLogger = false;
+
+    /**
+     * @param LoggerInterface $logger
+     */
     public function addLogger(LoggerInterface $logger)
     {
         $this->loggers[] = $logger;
+        $this->hasLogger = true;
     }
 
+    /**
+     * @param string $name
+     * @param string $host
+     * @param integer $port
+     *
+     * @return Connection
+     */
     public function createConnection($name, $host, $port)
     {
-        $options = array('host' => $host, 'port' => $port);
-        if (count($this->loggers)) {
+        $options = array(
+            'host' => $host,
+            'port' => $port
+        );
+
+        if ($this->hasLogger === true) {
             $loggers = $this->loggers;
-            $trace = function($type, $data) use($name, $loggers) {
+            $trace = function ($type, $data) use ($name, $loggers) {
                 foreach ($loggers as $logger) {
                     $logger->log($name, $type, $data);
                 }
